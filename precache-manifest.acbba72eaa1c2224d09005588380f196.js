06dbc0a8b1332bdd127ac4e53c050a5d8590906b
Laure Claret <laure.a.claret@gmail.com>
2020-06-12 16:38:15 +1000

    Updates

---
+++ precache-manifest.acbba72eaa1c2224d09005588380f196.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151a31fc2e886c08a43365af7f55147a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "97080e21a79f974125ce",
+    "url": "/portfolio/static/css/main.a4e7657f.chunk.css"
+  },
+  {
+    "revision": "7f2bd0cd58eaf1c96b4b",
+    "url": "/portfolio/static/js/2.87c30b90.chunk.js"
+  },
+  {
+    "revision": "8f1544b07245491ea591f336c2dcc0b8",
+    "url": "/portfolio/static/js/2.87c30b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97080e21a79f974125ce",
+    "url": "/portfolio/static/js/main.663c18fe.chunk.js"
+  },
+  {
+    "revision": "2896c7c99fec2b01eae7",
+    "url": "/portfolio/static/js/runtime-main.0cabc7df.js"
+  },
+  {
+    "revision": "3465b949d5a298e83d05cb4bdc0a6057",
+    "url": "/portfolio/static/media/event.3465b949.JPG"
+  },
+  {
+    "revision": "0b33bbe79a4ebff8b726b3956fe750de",
+    "url": "/portfolio/static/media/github.0b33bbe7.JPG"
+  },
+  {
+    "revision": "c8c1df495d32ee1e74cd95bc2a1716f4",
+    "url": "/portfolio/static/media/guitar.c8c1df49.JPG"
+  },
+  {
+    "revision": "dcbdf28830c3e365666fd9da83ca9f88",
+    "url": "/portfolio/static/media/jap.dcbdf288.JPG"
+  },
+  {
+    "revision": "f43ca2dc5006f1970b88d5627992410b",
+    "url": "/portfolio/static/media/meditation.f43ca2dc.JPG"
+  },
+  {
+    "revision": "896b7af02995bc2424ea63848e685eeb",
+    "url": "/portfolio/static/media/tracker.896b7af0.JPG"
+  }
+]);
