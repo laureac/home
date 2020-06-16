4dcc8194abbe7ddd634a1a68524f0ecfdc44cad4
Laure Claret <laure.a.claret@gmail.com>
2020-06-17 09:24:48 +1000

    Updates

---
+++ precache-manifest.cd07adbb0a5ff2dcffb70c95db1652c0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc5cf8165e28d81cab5ce54133923fc",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "6a954467cc3c9a182825",
+    "url": "/home/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "930569b6f7b6fdf224be",
+    "url": "/home/static/css/main.2a3ed0d7.chunk.css"
+  },
+  {
+    "revision": "6a954467cc3c9a182825",
+    "url": "/home/static/js/2.f4c78afc.chunk.js"
+  },
+  {
+    "revision": "8f1544b07245491ea591f336c2dcc0b8",
+    "url": "/home/static/js/2.f4c78afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "930569b6f7b6fdf224be",
+    "url": "/home/static/js/main.c9de4c92.chunk.js"
+  },
+  {
+    "revision": "be9c01c3fbce44b75f9a",
+    "url": "/home/static/js/runtime-main.3a2c65ad.js"
+  },
+  {
+    "revision": "3465b949d5a298e83d05cb4bdc0a6057",
+    "url": "/home/static/media/event.3465b949.JPG"
+  },
+  {
+    "revision": "0b33bbe79a4ebff8b726b3956fe750de",
+    "url": "/home/static/media/github.0b33bbe7.JPG"
+  },
+  {
+    "revision": "c8c1df495d32ee1e74cd95bc2a1716f4",
+    "url": "/home/static/media/guitar.c8c1df49.JPG"
+  },
+  {
+    "revision": "dcbdf28830c3e365666fd9da83ca9f88",
+    "url": "/home/static/media/jap.dcbdf288.JPG"
+  },
+  {
+    "revision": "ca8aedbfc60c35e8c489aae8d524a8f2",
+    "url": "/home/static/media/login.ca8aedbf.JPG"
+  },
+  {
+    "revision": "f43ca2dc5006f1970b88d5627992410b",
+    "url": "/home/static/media/meditation.f43ca2dc.JPG"
+  },
+  {
+    "revision": "896b7af02995bc2424ea63848e685eeb",
+    "url": "/home/static/media/tracker.896b7af0.JPG"
+  },
+  {
+    "revision": "b559aa371ecc6a51c6ee169a71e8ec8b",
+    "url": "/home/static/media/video.b559aa37.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c3bd5952a0f3a9d419a4b1a20ac11a92.js"
+  "/home/precache-manifest.cd07adbb0a5ff2dcffb70c95db1652c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
